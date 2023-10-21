eddb9f71e43e42ae49c23a38236ea5cda9b5a1c6
Brandon Zomer <brandon.zomer@mohawkcollege.ca>
2023-10-21 22:37:27 +0100

    removed privacy view and controller method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,16 +32,5 @@ namespace GroupProjectDeployment.Controllers
             }
             return View(products);
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
