e32b929da92b443f0ea448c23502c0d1cd6914a7
Brandon Zomer <brandon.zomer@mohawkcollege.ca>
2023-10-23 16:23:39 +0100

    review owners and admins can now delete reviews

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -19,9 +19,9 @@ namespace GroupProjectDeployment.Controllers
         // POST: Reviews/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind(Prefix = "NewReview")] Review review)
         {
             review.Product = _context.Products.Find(review.Id);
@@ -35,24 +35,29 @@ namespace GroupProjectDeployment.Controllers
             return RedirectToAction(nameof(HomeController.Error), "Home");
         }
 
-        // POST: Reviews/Delete/5
-        [Authorize(Roles = "Admin")]
-        [HttpPost, ActionName("Delete")]
+        // POST: Reviews/Remove/5
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        [Authorize]
+        public async Task<IActionResult> Remove(Guid id)
         {
             if (_context.Reviews == null)
             {
                 return Problem("Entity set 'ApplicationDbContext.Reviews'  is null.");
             }
             var review = await _context.Reviews.FindAsync(id);
-            if (review != null)
+            //Only delete if user is the owner of the review or an admin
+            if (review != null
+                && (User.Identity.Name.Equals(review.Email)
+                || User.IsInRole("Admin")))
             {
                 _context.Reviews.Remove(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
             }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            TempData["ErrorMessage"] = "Error deleting review. You are either not the owner of this review or not an admin.";
+            return RedirectToAction("Error", "Home");
         }
 
         private bool ReviewExists(Guid id)
